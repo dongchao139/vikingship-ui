cfbfd0d7c50ef321a4d22c17134acebec7110347
donnchao <donnchao@outlook.com>
2020-08-27 00:04:28 +0800

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div style={{width: '85%',margin: '2rem auto'}}>
+      <header>
+        <h1>Hello World</h1>
+        <h2>Hello World</h2>
+        <h3>Hello World</h3>
+        <hr/>
+        <code>
+          const a = 'b';
+        </code>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
