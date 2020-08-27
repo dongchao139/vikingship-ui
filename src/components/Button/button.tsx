77165372bee2530a5275ac6115a384ade03be81f
donnchao <donnchao@outlook.com>
2020-08-27 23:11:00 +0800

    添加注释

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -22,6 +22,29 @@ interface BaseButtonProps {
   href?: string;
 }
 
+/**
+ * 不同的Button Type：
+ *   primary default danger（button元素） link-button（a元素）
+ * 不同的Button Size：
+ *   Normal Small Large
+ * Disabled状态
+ *   Disabled link-button disabled（a链接没有disabled属性，需要使用 .dissabled）
+ * 
+ * Button隐藏属性：
+ *   className、title、name、autofocus
+ * 
+ * 使用方法：
+ * <Button
+ *   size='lg'
+ *   type='primary'
+ *   disabled
+ *   href=''?
+ *   className=''?
+ *   autoFocus=''?
+ * >
+ *   Viking Button
+ * </Button>
+ */
 const Button: React.FC<BaseButtonProps> = (props) => {
   const { btnType, disabled, size, children, href } = props;
   // btn, btn-lg btn-primary
