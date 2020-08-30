2c4ed602752818aadfd9dcc23e6be120d2d8d8f2
donnchao <donnchao@outlook.com>
2020-08-30 14:49:54 +0800

    btn-styles

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,43 @@ function App() {
   return (
     <div style={{width: '85%',margin: '2rem auto'}}>
       <header>
-        <Button disabled>Hello</Button>
+        <Button btnType={ButtonType.Primary} disabled>Hello World</Button>
         <Button 
           btnType={ButtonType.Primary}
-          size={ButtonSize.Large}>Hello</Button>
-        <Button
-          disabled
-          btnType={ButtonType.Link}
-          href="http://www.baidu.com"
-          >Baidu Link</Button>
+          size={ButtonSize.Large}>Hello World</Button>
+        <Button 
+          btnType={ButtonType.Primary}
+          size={ButtonSize.Small}>Hello World</Button>
         <hr/>
+
+        <Button btnType={ButtonType.Danger} disabled>Hello World</Button>
+        <Button 
+          btnType={ButtonType.Danger}
+          size={ButtonSize.Large}>Hello World</Button>
+        <Button 
+          btnType={ButtonType.Danger}
+          size={ButtonSize.Small}>Hello World</Button>
+        <hr/>
+
+        <Button btnType={ButtonType.Default} disabled>Hello World</Button>
+        <Button 
+          btnType={ButtonType.Default}
+          size={ButtonSize.Large}>Hello World</Button>
+        <Button 
+          btnType={ButtonType.Default}
+          size={ButtonSize.Small}>Hello World</Button>
+        <hr/>       
+         
+        <Button btnType={ButtonType.Link} 
+          href="http://www.baidu.com" disabled>Hello World</Button>
+        <Button 
+          btnType={ButtonType.Link}
+          size={ButtonSize.Large}>Hello World</Button>
+        <Button 
+          btnType={ButtonType.Link}
+          size={ButtonSize.Small}>Hello World</Button>
+        <hr/>
+        
         <h1>Hello World</h1>
         <h2>Hello World</h2>
         <h3>Hello World</h3>
