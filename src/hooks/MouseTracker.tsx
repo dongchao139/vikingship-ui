2f0f2140789d4a6eac4a1fb703ccd1b795827cc0
donnchao <donnchao@outlook.com>
2020-09-05 20:46:59 +0800

    commit

--- src/hooks/MouseTracker.tsx
+++ src/hooks/MouseTracker.tsx
@@ -1,22 +1,22 @@
 import React, { useState, useEffect } from 'react'
 
 const MouseTracker: React.FC = () => {
-    const [positions, setPositions] = useState({ x: 0, y: 0 });
-    // 需要清除的副作用
-    useEffect(() => {
-        const updateMouse = (e: MouseEvent) => {
-            setPositions({ x: e.clientX, y: e.clientY });
-        }
-        document.addEventListener('mousemove', updateMouse);
-        return () => {
-            document.removeEventListener('mousemove', updateMouse);
-        }
-    }, []);
-    return (
-        <p>
-            X: {positions.x},Y: {positions.y}
-        </p>
-    )
+  const [positions, setPositions] = useState({ x: 0, y: 0 });
+  // 需要清除的副作用
+  useEffect(() => {
+    const updateMouse = (e: MouseEvent) => {
+      setPositions({ x: e.clientX, y: e.clientY });
+    }
+    document.addEventListener('mousemove', updateMouse);
+    return () => {
+      document.removeEventListener('mousemove', updateMouse);
+    }
+  }, []);
+  return (
+    <p>
+      X: {positions.x},Y: {positions.y}
+    </p>
+  )
 }
 
 export default MouseTracker;
