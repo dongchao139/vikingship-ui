60bfb2f869b0c0dc25af44770d4b5a428bf914be
donnc <donnchao@outlook.com>
2020-09-13 21:41:46 +0800

    1

--- src/components/Alert/alert.tsx
+++ src/components/Alert/alert.tsx
@@ -2,18 +2,18 @@ import React, { useState } from 'react';
 import classnames from 'classnames';
 
 export enum AlertType {
-    SUCCESS = 'success',
-    DEFAULT = 'default',
-    DANGER = 'danger',
-    WARNING = 'warning'
+  SUCCESS = 'success',
+  DEFAULT = 'default',
+  DANGER = 'danger',
+  WARNING = 'warning'
 }
 
 export interface IAlertProps {
-    title?: string;
-    closable?: boolean;
-    customClose?: string;
-    children?: React.ReactNode;
-    type: AlertType;
+  title?: string;
+  closable?: boolean;
+  customClose?: string;
+  children?: React.ReactNode;
+  type: AlertType;
 }
 
 /**
@@ -23,22 +23,22 @@ export interface IAlertProps {
  * </Alert>
  */
 const Alert = (props: IAlertProps) => {
-    const { title, closable = true, customClose = '关闭', type } = props;
+  const { title, closable = true, customClose = '关闭', type } = props;
 
-    const classes = classnames('alert', {
-        [`alert-${type}`]: type
-    });
+  const classes = classnames('alert', {
+    [`alert-${type}`]: type
+  });
 
-    const [visible, setVisible] = useState(true);
-    return (
-        visible ?
-            <div className={classes}>
-                {title ? <h4 className="alert-title">{title}</h4> : null}
-                <p className="alert-message">{props.children}</p>
-                {closable ? <i onClick={() => setVisible(false)}>{customClose}</i> : null}
-            </div>
-            : null
-    );
+  const [visible, setVisible] = useState(true);
+  return (
+    visible ?
+      <div className={classes}>
+        {title ? <h4 className="alert-title">{title}</h4> : null}
+        <p className="alert-message">{props.children}</p>
+        {closable ? <i onClick={() => setVisible(false)}>{customClose}</i> : null}
+      </div>
+      : null
+  );
 }
 
 export default Alert;
