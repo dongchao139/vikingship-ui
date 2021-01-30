0bc281dd429e34b1538a53c5a79a4d87231a9e7b
donnchao <donnchao@outlook.com>
2021-01-30 23:38:51 +0800

    complete inputTime

--- src/components/InputTime/inputTime.tsx
+++ src/components/InputTime/inputTime.tsx
@@ -23,7 +23,15 @@ function formateNumber(num: number) {
     return '' + (num);
   }
 function simpleTimeFormate(date: Date) {
-    return date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+    return formateNumber(date.getHours()) + ":" + formateNumber(date.getMinutes()) + ":" + formateNumber(date.getSeconds());
+}
+function parseTime(timeStr: string): Date {
+    var arr = timeStr.split(":");
+    const time = new Date();
+    time.setHours(parseInt(arr[0]));
+    time.setMinutes(parseInt(arr[1]));
+    time.setSeconds(parseInt(arr[2]));
+    return time;
 }
 const hours = ['00','01','02','03','04','05','06','07','08','09',
 '10','11','12','13','14','15','16','17','18','19','20','21','22','23'];
@@ -31,9 +39,11 @@ const miniutes: string[] = [];
 for(let i=0;i<60;i++) {
     miniutes.push(formateNumber(i));
 }
+const reg = /\d{2}:\d{2}:\d{2}/;
 export const InputTime: React.FC<InputTimeProps> = ({
     ...restprops
 }) => {
+    const [time, setTime] = useState<Date>(new Date());
     const [value, setValue] = useState<string>(simpleTimeFormate(new Date()));
     const [show, setShow] = useState<boolean>(false);
     const ref = useRef();
@@ -44,10 +54,60 @@ export const InputTime: React.FC<InputTimeProps> = ({
     function handleChange(e) {
         setValue(e.target.value);
     }
+    function handleHour(h: string) {
+        const newTime = new Date(time.getTime());
+        newTime.setHours(parseInt(h));
+        setTime(newTime);
+        let newValue = null;
+        if (value) {
+            newValue = h + value.substring(2);
+        } else {
+            newValue = h + ":00:00";
+        }
+        setValue(newValue);
+    }
+    function handleMinute(m: string) {
+        const newTime = new Date(time.getTime());
+        newTime.setMinutes(parseInt(m));
+        setTime(newTime);
+        let newValue = null;
+        if (value) {
+            newValue =  value.substr(0,3) + m + value.substr(5,3);
+        } else {
+            newValue = "00:" + m + ":00";
+        }
+        setValue(newValue);
+    }
+    function handleSecond(s: string) {
+        const newTime = new Date(time.getTime());
+        newTime.setSeconds(parseInt(s));
+        setTime(newTime);
+        let newValue = null;
+        if (value) {
+            newValue = value.substr(0,6) + s;
+        } else {
+            newValue = "00:00:" + s;
+        }
+        setValue(newValue);
+    }
+    function handleBlur(e) {
+        const value = e.target.value;
+        console.log(value);
+        if (reg.test(value)) {
+          setTime(parseTime(value));
+        } else {
+            setValue(simpleTimeFormate(time));
+        }
+      }
+      function handleNow() {
+          setTime(new Date());
+          setValue(simpleTimeFormate(new Date()));
+      }
     return (
         <Input {...restprops} value={value} 
           onChange={handleChange} inputRef={inputRef}
           onFocus={() => setShow(true)}
+          onBlur={handleBlur}
         >
           <Icon className="calendar-icon" icon="clock"></Icon>
           <Transition in={show} animation="zoom-in-top" timeout={250} wrapper={false}
@@ -55,22 +115,31 @@ export const InputTime: React.FC<InputTimeProps> = ({
               <div className='calendar' ref={ref}>
                 <div className='list list-hour'>
                     {hours.map(h => {
-                        return <div className="item">{h}</div>
+                        const cls = classnames('item', {
+                            'now': parseInt(h) === time.getHours()
+                        });
+                        return <div className={cls} onClick={() => handleHour(h)}>{h}</div>
                     })}
                 </div>
                 <div className='list list-bordered'>
                     {miniutes.map(m => {
-                        return <div className="item">{m}</div>
+                        const cls = classnames('item', {
+                            'now': parseInt(m) === time.getMinutes()
+                        });
+                        return <div className={cls} onClick={() => handleMinute(m)}>{m}</div>
                     })}
                 </div>
                 <div className='list'>
-                    {miniutes.map(m => {
-                        return <div className="item">{m}</div>
+                    {miniutes.map(s => {
+                        const cls = classnames('item', {
+                            'now': parseInt(s) === time.getSeconds()
+                        });
+                        return <div className={cls} onClick={() => handleSecond(s)}>{s}</div>
                     })}
                 </div>
                 <div className="calendar-footer">
-                  此刻
-                  <a className="confirm">确 定</a>
+                  <span onClick={handleNow}>此刻</span>
+                  <span className="confirm">确 定</span>
                 </div>
               </div>
           </Transition>
